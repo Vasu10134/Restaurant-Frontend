096806edb940ff2862356301176a6622cc2e58ef
Vasu Sahu <Vasu.sahu.8080@gmail.com>
2025-11-16 16:23:31 +0530

    Added Backend & DB Connection

---
+++ Backend/database/dbConnection.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+export const dbConnection = () => {
+    mongoose.connect(process.env.MONGO_URL, {
+        dbName: "restaurantApp",
+    })
+    .then(() => {
+        console.log("Database connected successfully");
+    })
+    .catch((err) => {
+        console.log(`Database connection failed, ${err}`);
+    });
+};
