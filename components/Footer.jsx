c8300af92e15728048c12c98a094999a6297d357
Vasu Sahu <Vasu.sahu.8080@gmail.com>
2025-11-16 21:09:42 +0530

    Added Ui Files

---
+++ components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Vasu Sahu</div>
+          <div className="right">
+            <p>Vijay Nagar, Indore, Madhya Pradesh</p>
+            <p>Open: 09:00 AM - 11:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Vasu Sahu</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Vasu Sahu.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
